e990baec516948e3e08bced9868571dabf479a7e
Andrea <andrea.hidalgo90@gmail.com>
2020-12-08 21:33:49 -0500

    made basic classes and set initial game structure

--- app.js
+++ app.js
@@ -4,13 +4,6 @@
 
 /* 
 
-***Basic Gameplay***
-My project will be a "choose your own adventure" style text-based game. 
-Players will have the choice to select a character class and set their character name
-Players will make choices on how to navigate an underground labyrinth, ocasionally fighting monsters. 
-Players' choices will actively affect the outcome of the game.
-If a player dies, the game will start over from the beginning.
-
 **Layout**
 //Start Screen
 -Game Title
@@ -29,8 +22,127 @@ If a player dies, the game will start over from the beginning.
 -Monster Information
 
 **Structure**
+Classes
+-Player
+constructor {
+    name
+    class
+    health
+    weapon/attack power
+    special ability/attack power
+    health potions }
+weapon()
+special()
+heal()
 
+-Monster
+constructor {
+    name
+    health
+    weapon/attack power
+    special ability/attack power *stretch }
+weapon()
+special() *stretch
+
+-Scenario
+constructor {
+    name
+    story: string that will display in DOM
+    option1: string that will display in DOM button
+    option2: string that will display in DOM button
+    monster: assigned monster if applicable }
+select1() => run scenario X
+select2() => run scenario X
+
+**Required Functions
+Display in DOM functions
+    displayStory
+    displayOpt1
+    displayOpt2
+    displayName
+    displayClass
+    displayHealth
+    displayWeapon
+    displaySpecial
+    displayPotions
+runScenario(name)
 
 */
 
 console.log('Hello World');
+console.log($);
+
+class Player {
+    constructor() {
+        this.name = "Player";
+        this.playerClass = "Knight";
+        this.weapon = "Sword";
+        this.weaponPower = 5;
+        this.health = 20;
+        this.healthPotions = 5;
+        this.potionPower = 5;
+    }
+    weaponAttack(enemy) {
+        enemy.health -= this.weaponPower
+        console.log(`${enemy.name} has been attacked. His health was reduced by ${this.weaponPower}`);
+    }
+    heal() {
+        if (this.healthPotions >= 1) {
+            this.health += this.potionPower;
+            console.log(`${this.name} has been healed`);
+        }
+    }
+}
+
+class Monster {
+    constructor(name, weapon, weaponPower, health) {
+        this.name = name;
+        this.weapon = weapon;
+        this.weaponPower = weaponPower;
+        this.health = health;
+    }
+    weaponAttack(enemy) {
+        enemy.health -= this.weaponPower
+        console.log(`${enemy.name} has been attacked. His health was reduced by ${this.weaponPower}`);
+    }
+}
+
+const displayStory = (scenario) => {
+    $('.storyDisplay').text(scenario.story);
+    console.log('story displayed');
+}
+
+const displayOptions = (scenario) => {
+    $('.optionOne').text(scenario.optionOne);
+    $('.optionTwo').text(scenario.optionTwo);
+    console.log('option one and two displayed');
+}
+
+const runScenario = (scenario) => {
+    console.log(`displaying ${scenario.type} scenario `);
+    displayStory(scenario);
+    displayOptions(scenario);
+}
+
+const startScenario = {
+    type: "story",
+    story: "ABC",
+    optionOne: "Run left",
+    selectOne: "",
+    optionTwo: "Run right",
+    selectTwo: ""
+}
+
+const scenarioOne = {
+    type: "story", 
+    story: "XYZ",
+    optionOne: "Go north",
+    selectOne: runScenario(startScenario),
+    optionTwo: "Go south",
+    selectTwo: "runScenario(scenarioTwo)"
+}
+
+
+runScenario(startScenario);
+
+
